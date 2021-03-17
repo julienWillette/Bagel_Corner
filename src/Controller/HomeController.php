7fe63544775c84858ffa94c4c1501fa224f0054e
julien willette <julien.willette@gmail.com>
2021-03-17 11:47:06 +0100

    add news

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use App\Entity\Bagels;
+use App\Repository\NewsRepository;
 use App\Repository\BagelsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(BagelsRepository $bagelsRepository): Response
+    public function index(BagelsRepository $bagels, NewsRepository $news): Response
     {
+        $bagels = $this->getDoctrine()
+        ->getRepository(Bagels::class)
+        ->findAll();
+        $news = $this->getDoctrine()
+        ->getRepository(News::class)
+        ->findAll();
+        
         return $this->render('home/index.html.twig', [
-            'bagels' => $bagelsRepository->findAll(),
+            'bagels' => $bagels,
+            'news' => $news
         ]);
     }
 }
